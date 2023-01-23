73a16327e75de7e051ed06504a585cd9d9dc1b19
sonadro <sondre.rooth@gmail.com>
2023-01-23 20:25:56 +0100

    Ghost display function

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,9 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        client.user.setPresence({
+            status: 'dnd',
+        });
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
