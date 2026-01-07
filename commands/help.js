b62ecf7727b12e007f81d4b4e2d2f6c2609854b3
juju <89987062+ZombieJuju@users.noreply.github.com>
2026-01-07 11:10:45 +0100

    trying to fix help command

--- commands/help.js
+++ commands/help.js
@@ -5,65 +5,54 @@ module.exports = {
         .setName('help')
         .setDescription('A small guide of all our commands.')
 
-        // posessions
         .addSubcommand(subcommand =>
             subcommand
                 .setName('posessions')
                 .setDescription('A list of all commands related to cursed posessions.'))
 
-        // equipment
         .addSubcommand(subcommand =>
             subcommand
                 .setName('equipment')
                 .setDescription('A list of all equipment commands.'))
 
-        // fun
         .addSubcommand(subcommand =>
             subcommand
                 .setName('fun')
                 .setDescription('A list of all fun commands.'))
 
-        // general
         .addSubcommand(subcommand =>
             subcommand
                 .setName('general')
                 .setDescription('A list of all general commands.'))
 
-        // ghost
         .addSubcommand(subcommand =>
             subcommand
                 .setName('ghost')
                 .setDescription('A list of all ghost commands.'))
 
-        // maps
         .addSubcommand(subcommand =>
             subcommand
                 .setName('maps')
                 .setDescription('A list of all map commands.'))
 
-        // impressum
         .addSubcommand(subcommand =>
             subcommand
                 .setName('impressum')
                 .setDescription('A list of who helped making this bot possible.'))
 
-        // rules
         .addSubcommand(subcommand =>
             subcommand
                 .setName('rules')
                 .setDescription('Please review our rules, if you haven\'t already.')),
 
-    // execute command
     async execute(interaction) {
         await interaction.deferReply();
         const subcommand = interaction.options.getSubcommand();
 
-        // all menus has this base-embed
         const helpEmbed = new EmbedBuilder()
-            .setColor('#FF5A5A')
+            .setColor('#FF5A5A');
 
         if (subcommand === 'posessions') {
-            // update embed
             helpEmbed
                 .setTitle('Cursed Posessions')
                 .setDescription('Overview of all Cursed Posessions')
@@ -74,12 +63,11 @@ module.exports = {
                     { name: 'Haunted Mirror', value: 'Command: /haunted-mirror' },
                     { name: 'Summoning Circle', value: 'Command: /summoning-circle' },
                     { name: 'Tarot Cards', value: 'Command: /tarot-cards'},
-                    { name: 'Monkey Paw', value: 'Command: /monkey-paw'},
-                ).setFooter({
-                    text: 'Side note: It is guaranteed to get 1 cursed object in every game with standard difficulties. However this can be changed with custom difficulty!',
-                })
+                    { name: 'Monkey Paw', value: 'Command: /monkey-paw'}
+                )
+                .setFooter({ text: 'Side note: It is guaranteed to get 1 cursed object in every game with standard difficulties. However this can be changed with custom difficulty!' });
+
         } else if (subcommand === 'equipment') {
-            // update embed
             helpEmbed
                 .setTitle('Equipment')
                 .setDescription('Overview of all equipment')
@@ -105,12 +93,11 @@ module.exports = {
                     { name: 'Flashlight', value: 'Command: /flashlight', inline: true },
                     { name: 'Thermometer', value: 'Command: /thermometer', inline: true },
                     { name: 'Tripod', value: 'Command: /tripod', inline: true },
-                    { name: 'Soundrecorder', value: 'Command: /sound-recorder', inline: true },
-                ).setFooter({
-                    text: 'Boooo!',
-                })
+                    { name: 'Soundrecorder', value: 'Command: /sound-recorder', inline: true }
+                )
+                .setFooter({ text: 'Boooo!' });
+
         } else if (subcommand === 'fun') {
-            // update embed
             helpEmbed
                 .setTitle('Fun commands! :D')
                 .setDescription('Overview of all fun commands')
@@ -118,12 +105,11 @@ module.exports = {
                     { name: '/roulette', value: 'Draw a random tarot card!' },
                     { name: '/challenge', value: 'The bot gives you a challenge.\nAlso try: /challenge itemban, /challenge language, /challenge only3' },
                     { name: '/rollitem', value: 'Rolls a random item for you. Can be used in for example photocam challenge.' },
-                    { name: '/difficulty', value: 'The bot will choose a random difficulty for you!' },
-                ).setFooter({
-                    text: 'Boooo!',
-                })
+                    { name: '/difficulty', value: 'The bot will choose a random difficulty for you!' }
+                )
+                .setFooter({ text: 'Boooo!' });
+
         } else if (subcommand === 'general') {
-            // update embed
             helpEmbed
                 .setTitle('General Information')
                 .setDescription('Overview of general commands or information')
@@ -137,18 +123,16 @@ module.exports = {
                     { name: '/ghost-photo', value: 'Some info about ghost photos', inline: true },
                     { name: '/gtk', value: 'Stuff that is nice to know', inline: true },
                     { name: '/interaction-photo', value: 'Tips for taking photos of ghost interactions', inline: true },
-                    { name: '/posession-photo', value: 'Tips for taking photos of cursed posessions', inline: true },
+                    { name: '/posession-photo', value: 'Tips about taking photos of cursed posessions', inline: true },
                     { name: '/media', value: 'Info about media and the rewards', inline: true },
                     { name: '/hunt-sanity', value: 'At what sanity ghosts can hunt you', inline: true },
                     { name: '/smudging', value: 'What you should know about smudging', inline: true },
                     { name: '/dirty-water-photo', value: 'Tips about taking dirty water photos', inline: true },
-                    { name: '/video', value: 'What you should know about capturing videos', inline: true },
+                    { name: '/video', value: 'What you should know about capturing videos', inline: true }
                 )
-                .setFooter({
-                    text: 'Boooo!',
-               })
+                .setFooter({ text: 'Boooo!' });
+
         } else if (subcommand === 'ghost') {
-            // update embed
             helpEmbed
                 .setTitle('Ghosts')
                 .setDescription('Overview of all Ghosts')
@@ -180,12 +164,10 @@ module.exports = {
                     { name: 'Dayan', value: 'Command: /ghost dayan', inline: true},
                     { name: 'Gallu', value: 'Command: /ghost gallu', inline: true},
                     { name: 'Obambo', value: 'Command: /ghost obambo', inline: true}
-                )  
-                .setFooter({
-                    text: 'Boooo!',
-            })
+                )
+                .setFooter({ text: 'Boooo!' });
+
         } else if (subcommand === 'maps') {
-            // update embed
             helpEmbed
                 .setTitle('Maps')
                 .setDescription('Overview of all the Maps')
@@ -205,22 +187,18 @@ module.exports = {
                     { name: 'Point Hope', value: 'Command: /point-hope', inline: true},
                     { name: "Nell's Diner", value: 'Command: /nells-diner', inline: true}
                 )
-                .setFooter({
-                    text: 'Boooo!',
-            })
+                .setFooter({ text: 'Boooo!' });
+
         } else if (subcommand === 'impressum') {
-            // update embed
             helpEmbed
                 .setTitle('Impressum')
-                .setDescription('\nInfo about ghosts, events and general stuff - Insym: \nhttps://www.youtube.com/watch?v=6c00T2duoEg \nhttps://www.twitch.tv/insym \n  \nMaps w/ cursed posession locations - Fantismal: \nhttps://imgur.com/a/iEI0tJo \n  \nEquipment, cursed objects, events: \nhttps://phasmophobia.fandom.com/wiki/Main_Page \n  \nMoroi, Deogen & Thaye - Psycho: \n https://www.youtube.com/watch?v=VjbCps35D5Q \nhttps://www.twitch.tv/psychohypnotic \n \nCoded by: \nNullody#5271 & juju~#2239 \n  \nSupport: https://discord.gg/wTTQSkCcPE')
-                .setFooter({
-                    text: 'Boooo!',
-            })
+                .setDescription('Info about ghosts, events, maps, and bot coding credits...')
+                .setFooter({ text: 'Boooo!' });
+
         } else if (subcommand === 'rules') {
-            // update embed
             helpEmbed
                 .setTitle('Rules')
-                .setDescription('With reacting to this message you agree with all rules and accept consequences when following them. <:ghostii:1034051076183310347>')
+                .setDescription('With reacting to this message you agree with all rules and accept consequences...')
                 .addFields(
                     { name: 'Rule 1', value: 'Treat everyone with respect. Absolutely no harassment, toxity, witch hunting, sexism, racism, or hate speech will be tolerated.'},
                     { name: 'Rule 2', value: 'No NSFW or obscene content. This includes text, images, or links featuring nudity, sex, hard violence, or other graphically disturbing content.'},
@@ -228,13 +206,11 @@ module.exports = {
                     { name: 'Rule 4', value: 'No swearing. We want to keep the server family friendly, so everyone can be here!'},
                     { name: 'Rule 5', value: 'Keep the chat English so everyone can understand!'},
                     { name: 'Rule 6', value: "Don't annoy members, stay respectful and have common sense. Things that aren't okay to do irl aren't okay to do here too!"},
-                    { name: 'Rule 7', value: 'No trolling or bad behaviour in games. Keep it Friendly and supportive no matter who you play with and how good they are in the game!'})
-                .setFooter({
-                    text: 'Enjoy your stay!',
-                })
+                    { name: 'Rule 7', value: 'No trolling or bad behaviour in games. Keep it Friendly and supportive no matter who you play with and how good they are in the game!'}
+                )
+                .setFooter({ text: 'Enjoy your stay!' });
         }
 
-        // once embed has been updated, send it!
         await interaction.editReply({ embeds: [helpEmbed] });
     }
 }
